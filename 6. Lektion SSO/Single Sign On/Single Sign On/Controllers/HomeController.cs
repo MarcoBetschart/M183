9cd8941dbfa8d69dac721e94159b4aa620c86f92
Marco Betschart <marco.betschart98@gmail.com>
2017-10-06 08:29:07 +0200

    SSO fertig gestellt

--- 6. Lektion SSO/Single Sign On/Single Sign On/Controllers/HomeController.cs
+++ 6. Lektion SSO/Single Sign On/Single Sign On/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +15,30 @@ namespace Single_Sign_On.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult SSoTokenSignIn()
+        {
+            var id_token = Request["idtoken"];
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token);
+
+            var postData = "id_token" + id_token;
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using(var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            
+            return Json(responseString);
         }
     }
 }
