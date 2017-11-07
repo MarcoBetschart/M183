5be32158a2131d9f51aa349e2b07c615dedf5908
Marco Betschart <marco.betschart98@gmail.com>
2017-11-07 19:35:26 +0100

    Verbesserungen 2-Factor Auth

--- 4. Lektion 2-Factor Ath/2-Factor Auth TOTP/2-Factor Auth TOTP/Controllers/HomeController.cs
+++ 4. Lektion 2-Factor Ath/2-Factor Auth TOTP/2-Factor Auth TOTP/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using _2_Factor_Auth_TOTP.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,40 +17,59 @@ namespace _2_Factor_Auth_TOTP.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(User users)
+        public ActionResult Login()
         {
             if (ModelState.IsValid)
             {
-                if (users.UserName == "Marco" && users.Password == "Test")
+                if (Request["username"] == "Marco" &&  Request["password"] == "Test")
                 {
-                    if (users.UseSMS)
+                    //if (users.UseSMS)
+                    //{
+                    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    var stringChars = new char[6];
+                    var random = new Random();
+
+                    for (int i = 0; i < stringChars.Length; i++)
                     {
-                        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-                        var stringChars = new char[6];
-                        var random = new Random();
-
-                        for (int i = 0; i < stringChars.Length; i++)
-                        {
-                            stringChars[i] = chars[random.Next(chars.Length)];
-                        }
-
-                        var finalString = new String(stringChars);
-
-
-                        //Send(new SMSRequest
-                        //{
-                        //    from = "0799456133",
-                        //    to = "0799456133",
-                        //    text = finalString
-                        //});
-                    }
-                    {
-                        var key = OtpNet.KeyGeneration.GenerateRandomKey(6);
-                        
+                        stringChars[i] = chars[random.Next(chars.Length)];
                     }
 
-                }
-                {
+                    var finalString = new String(stringChars);
+
+
+
+					var request = (HttpWebRequest)WebRequest.Create("https://rest.nexmo.com/sms/json");
+
+					var postData = "api_key=56435b83";
+					postData += "&api_secret=3350e362df8ec70";
+					postData += "&to=41799456133";
+					postData += "&from=\"\"NEXMO\"\"";
+					postData += "&text=\"" + finalString + "\"";
+					var data = Encoding.ASCII.GetBytes(postData);
+
+					request.Method = "POST";
+					request.ContentType = "application/x-www-form-urlencoded";
+					request.ContentLength = data.Length;
+
+					using(var stream = request.GetRequestStream())
+					{
+						stream.Write(data, 0, data.Length);
+					}
+
+					var response = (HttpWebResponse)request.GetResponse();
+
+					var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+					ViewBag.Message = responseString;
+
+					//}
+					//else{
+					//    var key = OtpNet.KeyGeneration.GenerateRandomKey(6);
+
+					//}
+
+				}
+                else{
                     ModelState.AddModelError("", "Invalid username and/or password");
                 }
             }
